836f0c93be2a9fcd9ca1565e57f9745ec249558f
Ahmet Özdil <120733428+ozdilAhmet@users.noreply.github.com>
2023-10-13 13:00:12 +0300

    Add files via upload

---
+++ egzersiz6/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Kullanicinin girdigi bir sayiyi 1'den baslayarak o sayiya kadar olan sayilarin toplamini
+//bulan programi yaziniz.
+int main()
+{
+ int a;
+
+ printf("Lutfen bir sayi giriniz.\n");
+ scanf("%d",&a);
+
+ printf("%d",a*(a+1)/2);
+
+
+
+    return 0;
+}
